53d177b5bef87fce5d21946a418a7772f33086db
Nir Cohen <nircohen.biz@gmail.com>
2023-11-15 22:21:14 +0200

    Add ball3 swapping ball1 and ball2 color and size

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const elBall1 = document.querySelector('.ball1')
+const elBall2 = document.querySelector('.ball2')
+const elBall3 = document.querySelector('.ball3')
+
 function onBallClick(elBall, maxDiameter) {
   // Get the computed styles of the element
   const computedStyle = window.getComputedStyle(elBall)
@@ -10,10 +14,13 @@ function onBallClick(elBall, maxDiameter) {
   if (isNaN(currWidth)) currWidth = 50 // Assuming default width to start with
   if (isNaN(currHeight)) currHeight = 50 // Assuming default height to start with
 
-  currWidth = currWidth >= maxDiameter ? 100 : currWidth
-  currHeight = currHeight >= maxDiameter ? 100 : currHeight
-
   const randomSize = getRandomInt(20, 61)
+
+  currWidth =
+    currWidth + randomSize >= maxDiameter ? 100 : currWidth + randomSize
+  currHeight =
+    currHeight + randomSize >= maxDiameter ? 100 : currHeight + randomSize
+
   const randColor = '#' + getRandomColor()
 
   elBall.style.width = currWidth + randomSize + 'px'
@@ -22,4 +29,16 @@ function onBallClick(elBall, maxDiameter) {
   elBall.innerText = elBall.style.width
 
   elBall.style.backgroundColor = randColor
+
+  if (elBall.classList.contains('ball3')) {
+    tempColor = elBall2.style.backgroundColor
+    elBall2.style.backgroundColor = elBall1.style.backgroundColor
+    elBall1.style.backgroundColor = tempColor
+
+    tempSize = elBall2.style.width
+    elBall2.style.width = elBall1.style.width
+    elBall2.style.height = elBall1.style.width
+    elBall1.style.width = tempSize
+    elBall1.style.height = tempSize
+  }
 }
