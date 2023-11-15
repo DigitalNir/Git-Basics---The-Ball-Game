5f2cd5ec1df71018276a063c38189f840c2d9b5a
Nir Cohen <nircohen.biz@gmail.com>
2023-11-15 18:38:27 +0200

    Change onBallClick to increase ball size

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-function onBallClick() {
-  console.log('Ball clicked')
+function onBallClick(elBall) {
+  // Get the computed styles of the element
+  const computedStyle = window.getComputedStyle(elBall)
+
+  // Parse the current width and height to integers
+  let currWidth = parseInt(computedStyle.width)
+  let currHeight = parseInt(computedStyle.height)
+
+  // Check if the currentWidth and currentHeight are numbers, if not assign a default value
+  if (isNaN(currWidth)) currWidth = 50 // Assuming default width to start with
+  if (isNaN(currHeight)) currHeight = 50 // Assuming default height to start with
+
+  elBall.style.width = currWidth + 50 + 'px'
+  elBall.style.height = currHeight + 50 + 'px'
+
+  elBall.innerText = elBall.style.width
 }
